4aecc2ccae6b9c75201074ac999434f9ce8ce7b5
Janos Bonic <86970079+janosdebugs@users.noreply.github.com>
2022-09-28 08:32:03 +0200

    Added TypedScopeSchema

--- schema/scope.go
+++ schema/scope.go
@@ -1,6 +1,7 @@
 package schema
 
 import (
+	"fmt"
 	"reflect"
 )
 
@@ -70,4 +71,45 @@ func (s ScopeSchema) Objects() map[string]*ObjectSchema {
 
 func (s ScopeSchema) Root() string {
 	return s.RootValue
+}
+
+// NewTypedScopeSchema returns a new scope that is typed.
+func NewTypedScopeSchema[T any](rootObject *ObjectSchema, objects ...*ObjectSchema) *TypedScopeSchema[T] {
+	var defaultValue T
+	if reflect.TypeOf(defaultValue) != rootObject.ReflectedType() {
+		panic(BadArgumentError{
+			Message: fmt.Sprintf(
+				"Incorrect type definition: type %T does not match the root object type of %s",
+				defaultValue,
+				rootObject.ReflectedType().Name(),
+			),
+		})
+	}
+
+	return &TypedScopeSchema[T]{
+		*NewScopeSchema(rootObject, objects...),
+	}
+}
+
+// TypedScopeSchema is a typed variant of the ScopeSchema, allowing for direct type use. This should not be used in full
+// schema definitions as the type parameter will prevent it from being added to lists thanks to the simplistic
+// generics system in Go.
+type TypedScopeSchema[T any] struct {
+	ScopeSchema `json:",inline"`
+}
+
+func (t TypedScopeSchema[T]) UnserializeType(data any) (result T, err error) {
+	untypedResult, err := t.Unserialize(data)
+	if err != nil {
+		return result, err
+	}
+	return untypedResult.(T), nil
+}
+
+func (t TypedScopeSchema[T]) ValidateType(data T) error {
+	return t.Validate(data)
+}
+
+func (t TypedScopeSchema[T]) SerializeType(data T) (any, error) {
+	return t.Serialize(data)
 }
