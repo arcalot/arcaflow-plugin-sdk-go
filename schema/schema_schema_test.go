0523db3fffa2768257f947d711a6a18f5c1885d6
Janos Bonic <86970079+janosdebugs@users.noreply.github.com>
2023-04-11 12:05:29 +0200

    Exposing step output schema

--- schema/schema_schema_test.go
+++ schema/schema_schema_test.go
@@ -70,3 +70,37 @@ func TestSchemaUnserializationSuperScoped(t *testing.T) {
 	nameType := unserializedData.StepsValue["hello-world"].InputValue.Objects()["InputParams"].Properties()["name"].Type().(*schema.OneOfSchema[string])
 	assertEqual(t, nameType.Types()["fullname"].TypeID(), schema.TypeIDScope)
 }
+
+func TestStepOutputSchema(t *testing.T) {
+	stepOutputSchema := schema.DescribeStepOutput()
+	unserializedStepOutput, err := stepOutputSchema.Unserialize(map[string]any{
+		"schema": map[string]any{
+			"root": "A",
+			"objects": map[string]any{
+				"A": map[string]any{
+					"id": "A",
+					"properties": map[string]any{
+						"foo": map[string]any{
+							"type": map[string]any{
+								"type_id": "string",
+							},
+							"required": true,
+						},
+					},
+				},
+			},
+		},
+	})
+	if err != nil {
+		t.Fatalf("%v", err)
+	}
+	unserializedStepOutputOutput, err := unserializedStepOutput.(*schema.StepOutputSchema).Unserialize(map[string]any{
+		"foo": "bar",
+	})
+	if err != nil {
+		t.Fatalf("%v", err)
+	}
+	if unserializedStepOutputOutput.(map[string]any)["foo"] != "bar" {
+		t.Fatalf("Incorrect unserialized output: %s", unserializedStepOutputOutput.(map[any]any)["foo"])
+	}
+}
