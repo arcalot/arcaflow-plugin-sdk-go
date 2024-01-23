9f74041443a9095d2c72873bf9eb40ec4915fd98
Matthew F Leader <mleader@redhat.com>
2024-01-23 10:38:43 -0500

    add a test

--- schema/oneof_string_test.go
+++ schema/oneof_string_test.go
@@ -4,221 +4,356 @@
 package schema_test
 
 import (
-	"encoding/json"
-	"go.arcalot.io/assert"
-	"testing"
+    "encoding/json"
+    "go.arcalot.io/assert"
+    "testing"
 
-	"go.flow.arcalot.io/pluginsdk/schema"
+    "go.flow.arcalot.io/pluginsdk/schema"
 )
 
 var oneOfStringTestObjectAProperties = map[string]*schema.PropertySchema{
-	"s": schema.NewPropertySchema(
-		schema.NewOneOfStringSchema[any](
-			map[string]schema.Object{
-				"B": schema.NewRefSchema("B", nil),
-				"C": schema.NewRefSchema("C", nil),
-			},
-			"_type",
-		),
-		nil,
-		true,
-		nil,
-		nil,
-		nil,
-		nil,
-		nil,
-	),
+    "s": schema.NewPropertySchema(
+        schema.NewOneOfStringSchema[any](
+            map[string]schema.Object{
+                "B": schema.NewRefSchema("B", nil),
+                "C": schema.NewRefSchema("C", nil),
+            },
+            "_type",
+        ),
+        nil,
+        true,
+        nil,
+        nil,
+        nil,
+        nil,
+        nil,
+    ),
 }
 
 // Differs in that the discriminator field is different.
 var oneOfStringTestObjectAbProperties = map[string]*schema.PropertySchema{
-	"s": schema.NewPropertySchema(
-		schema.NewOneOfStringSchema[any](
-			map[string]schema.Object{
-				"B": schema.NewRefSchema("B", nil),
-				"C": schema.NewRefSchema("C", nil),
-			},
-			"_difftype",
-		),
-		nil,
-		true,
-		nil,
-		nil,
-		nil,
-		nil,
-		nil,
-	),
+    "s": schema.NewPropertySchema(
+        schema.NewOneOfStringSchema[any](
+            map[string]schema.Object{
+                "B": schema.NewRefSchema("B", nil),
+                "C": schema.NewRefSchema("C", nil),
+            },
+            "_difftype",
+        ),
+        nil,
+        true,
+        nil,
+        nil,
+        nil,
+        nil,
+        nil,
+    ),
 }
 
 // Differs in that a key doesn't match.
 var oneOfStringTestObjectAcProperties = map[string]*schema.PropertySchema{
-	"s": schema.NewPropertySchema(
-		schema.NewOneOfStringSchema[any](
-			map[string]schema.Object{
-				"B": schema.NewRefSchema("B", nil),
-				"D": schema.NewRefSchema("C", nil),
-			},
-			"_type",
-		),
-		nil,
-		true,
-		nil,
-		nil,
-		nil,
-		nil,
-		nil,
-	),
+    "s": schema.NewPropertySchema(
+        schema.NewOneOfStringSchema[any](
+            map[string]schema.Object{
+                "B": schema.NewRefSchema("B", nil),
+                "D": schema.NewRefSchema("C", nil),
+            },
+            "_type",
+        ),
+        nil,
+        true,
+        nil,
+        nil,
+        nil,
+        nil,
+        nil,
+    ),
 }
 
 // Differs in that a oneof schema doesn't match.
 var oneOfStringTestObjectAdProperties = map[string]*schema.PropertySchema{
-	"s": schema.NewPropertySchema(
-		schema.NewOneOfStringSchema[any](
-			map[string]schema.Object{
-				"B": schema.NewRefSchema("B", nil),
-				"C": schema.NewRefSchema("D", nil),
-			},
-			"_type",
-		),
-		nil,
-		true,
-		nil,
-		nil,
-		nil,
-		nil,
-		nil,
-	),
+    "s": schema.NewPropertySchema(
+        schema.NewOneOfStringSchema[any](
+            map[string]schema.Object{
+                "B": schema.NewRefSchema("B", nil),
+                "C": schema.NewRefSchema("D", nil),
+            },
+            "_type",
+        ),
+        nil,
+        true,
+        nil,
+        nil,
+        nil,
+        nil,
+        nil,
+    ),
 }
 
 var oneOfStringTestObjectASchema = schema.NewScopeSchema(
-	schema.NewObjectSchema(
-		"A",
-		oneOfStringTestObjectAProperties,
-	),
-	oneOfTestBSchema,
-	oneOfTestCSchema,
+    schema.NewObjectSchema(
+        "A",
+        oneOfStringTestObjectAProperties,
+    ),
+    oneOfTestBSchema,
+    oneOfTestCSchema,
 )
 
 var oneOfStringTestObjectAbSchema = schema.NewScopeSchema(
-	schema.NewObjectSchema(
-		"A",
-		oneOfStringTestObjectAbProperties,
-	),
-	oneOfTestBSchema,
-	oneOfTestCSchema,
-	oneOfTestDSchema,
+    schema.NewObjectSchema(
+        "A",
+        oneOfStringTestObjectAbProperties,
+    ),
+    oneOfTestBSchema,
+    oneOfTestCSchema,
+    oneOfTestDSchema,
 )
 var oneOfStringTestObjectAcSchema = schema.NewScopeSchema(
-	schema.NewObjectSchema(
-		"A",
-		oneOfStringTestObjectAcProperties,
-	),
-	oneOfTestBSchema,
-	oneOfTestCSchema,
-	oneOfTestDSchema,
+    schema.NewObjectSchema(
+        "A",
+        oneOfStringTestObjectAcProperties,
+    ),
+    oneOfTestBSchema,
+    oneOfTestCSchema,
+    oneOfTestDSchema,
 )
 var oneOfStringTestObjectAdSchema = schema.NewScopeSchema(
-	schema.NewObjectSchema(
-		"A",
-		oneOfStringTestObjectAdProperties,
-	),
-	oneOfTestBSchema,
-	oneOfTestCSchema,
-	oneOfTestDSchema,
+    schema.NewObjectSchema(
+        "A",
+        oneOfStringTestObjectAdProperties,
+    ),
+    oneOfTestBSchema,
+    oneOfTestCSchema,
+    oneOfTestDSchema,
 )
 
 var oneOfStringTestObjectAType = schema.NewScopeSchema(
-	schema.NewStructMappedObjectSchema[oneOfTestObjectA](
-		"A",
-		oneOfStringTestObjectAProperties,
-	),
-	oneOfTestBMappedSchema,
-	oneOfTestCMappedSchema,
+    schema.NewStructMappedObjectSchema[oneOfTestObjectA](
+        "A",
+        oneOfStringTestObjectAProperties,
+    ),
+    oneOfTestBMappedSchema,
+    oneOfTestCMappedSchema,
 )
 
 func TestOneOfStringUnserialization(t *testing.T) {
-	data := `{
+    data := `{
 	"s": {
 		"_type": "B",
 		"message": "Hello world!"
 	}
 }`
-	var input any
-	assert.NoError(t, json.Unmarshal([]byte(data), &input))
-	unserializedData, err := oneOfStringTestObjectAType.Unserialize(input)
-	assert.NoError(t, err)
-	assert.Equals(t, unserializedData.(oneOfTestObjectA).S.(oneOfTestObjectB).Message, "Hello world!")
+    var input any
+    assert.NoError(t, json.Unmarshal([]byte(data), &input))
+    unserializedData, err := oneOfStringTestObjectAType.Unserialize(input)
+    assert.NoError(t, err)
+    assert.Equals(t, unserializedData.(oneOfTestObjectA).S.(oneOfTestObjectB).Message, "Hello world!")
 }
 
 func TestOneOfStringCompatibilityValidation(t *testing.T) {
-	// The ones with NoError are matching schemas
-	// All others have one thing that differs, so should error.
-	assert.NoError(t, oneOfStringTestObjectASchema.ValidateCompatibility(oneOfStringTestObjectASchema))
-	assert.Error(t, oneOfStringTestObjectASchema.ValidateCompatibility(oneOfStringTestObjectAbSchema))
-	assert.Error(t, oneOfStringTestObjectASchema.ValidateCompatibility(oneOfStringTestObjectAcSchema))
-	assert.Error(t, oneOfStringTestObjectASchema.ValidateCompatibility(oneOfStringTestObjectAdSchema))
-	assert.Error(t, oneOfStringTestObjectAbSchema.ValidateCompatibility(oneOfStringTestObjectASchema))
-	assert.NoError(t, oneOfStringTestObjectAbSchema.ValidateCompatibility(oneOfStringTestObjectAbSchema))
-	assert.Error(t, oneOfStringTestObjectAbSchema.ValidateCompatibility(oneOfStringTestObjectAcSchema))
-	assert.Error(t, oneOfStringTestObjectAbSchema.ValidateCompatibility(oneOfStringTestObjectAdSchema))
-	assert.Error(t, oneOfStringTestObjectAcSchema.ValidateCompatibility(oneOfStringTestObjectASchema))
-	assert.Error(t, oneOfStringTestObjectAcSchema.ValidateCompatibility(oneOfStringTestObjectAbSchema))
-	assert.NoError(t, oneOfStringTestObjectAcSchema.ValidateCompatibility(oneOfStringTestObjectAcSchema))
-	assert.Error(t, oneOfStringTestObjectAcSchema.ValidateCompatibility(oneOfStringTestObjectAdSchema))
-	assert.Error(t, oneOfStringTestObjectAdSchema.ValidateCompatibility(oneOfStringTestObjectASchema))
-	assert.Error(t, oneOfStringTestObjectAdSchema.ValidateCompatibility(oneOfStringTestObjectAbSchema))
-	assert.Error(t, oneOfStringTestObjectAdSchema.ValidateCompatibility(oneOfStringTestObjectAcSchema))
-	assert.NoError(t, oneOfStringTestObjectAdSchema.ValidateCompatibility(oneOfStringTestObjectAdSchema))
+    // The ones with NoError are matching schemas
+    // All others have one thing that differs, so should error.
+    assert.NoError(t, oneOfStringTestObjectASchema.ValidateCompatibility(oneOfStringTestObjectASchema))
+    assert.Error(t, oneOfStringTestObjectASchema.ValidateCompatibility(oneOfStringTestObjectAbSchema))
+    assert.Error(t, oneOfStringTestObjectASchema.ValidateCompatibility(oneOfStringTestObjectAcSchema))
+    assert.Error(t, oneOfStringTestObjectASchema.ValidateCompatibility(oneOfStringTestObjectAdSchema))
+    assert.Error(t, oneOfStringTestObjectAbSchema.ValidateCompatibility(oneOfStringTestObjectASchema))
+    assert.NoError(t, oneOfStringTestObjectAbSchema.ValidateCompatibility(oneOfStringTestObjectAbSchema))
+    assert.Error(t, oneOfStringTestObjectAbSchema.ValidateCompatibility(oneOfStringTestObjectAcSchema))
+    assert.Error(t, oneOfStringTestObjectAbSchema.ValidateCompatibility(oneOfStringTestObjectAdSchema))
+    assert.Error(t, oneOfStringTestObjectAcSchema.ValidateCompatibility(oneOfStringTestObjectASchema))
+    assert.Error(t, oneOfStringTestObjectAcSchema.ValidateCompatibility(oneOfStringTestObjectAbSchema))
+    assert.NoError(t, oneOfStringTestObjectAcSchema.ValidateCompatibility(oneOfStringTestObjectAcSchema))
+    assert.Error(t, oneOfStringTestObjectAcSchema.ValidateCompatibility(oneOfStringTestObjectAdSchema))
+    assert.Error(t, oneOfStringTestObjectAdSchema.ValidateCompatibility(oneOfStringTestObjectASchema))
+    assert.Error(t, oneOfStringTestObjectAdSchema.ValidateCompatibility(oneOfStringTestObjectAbSchema))
+    assert.Error(t, oneOfStringTestObjectAdSchema.ValidateCompatibility(oneOfStringTestObjectAcSchema))
+    assert.NoError(t, oneOfStringTestObjectAdSchema.ValidateCompatibility(oneOfStringTestObjectAdSchema))
 }
 
 func TestOneOfStringCompatibilityMapValidation(t *testing.T) {
-	validWithObjectB := map[string]any{
-		"s": map[string]any{
-			"_type": "B",
-			// object B fields
-			"message": "test",
-		},
-	}
-	validWithObjectC := map[string]any{
-		"s": map[string]any{
-			"_type": "C",
-			// object B fields
-			"m": "test",
-		},
-	}
-	invalidDiscriminatorType := map[string]any{
-		"s": map[string]any{
-			"_type": 1,
-			// object B fields
-			"message": "test",
-		},
-	}
-	invalidDiscriminator := map[string]any{
-		"s": map[string]any{
-			"wrongdiscriminator": "B",
-			// object B fields
-			"message": "test",
-		},
-	}
+    validWithObjectB := map[string]any{
+        "s": map[string]any{
+            "_type": "B",
+            // object B fields
+            "message": "test",
+        },
+    }
+    validWithObjectC := map[string]any{
+        "s": map[string]any{
+            "_type": "C",
+            // object B fields
+            "m": "test",
+        },
+    }
+    invalidDiscriminatorType := map[string]any{
+        "s": map[string]any{
+            "_type": 1,
+            // object B fields
+            "message": "test",
+        },
+    }
+    invalidDiscriminator := map[string]any{
+        "s": map[string]any{
+            "wrongdiscriminator": "B",
+            // object B fields
+            "message": "test",
+        },
+    }
 
-	combinedMapAndSchema := map[string]any{
-		"s": map[string]any{
-			"_type": "B",
-			// object B fields, but with a schema instead of a value
-			"message": schema.NewStringSchema(nil, nil, nil),
-		},
-	}
-	combinedMapAndInvalidSchema := map[string]any{
-		"s": map[string]any{
-			"_type": "B",
-			// object B fields, but with a schema instead of a value
-			"message": schema.NewIntSchema(nil, nil, nil),
-		},
-	}
-	assert.NoError(t, oneOfStringTestObjectASchema.ValidateCompatibility(validWithObjectB))
-	assert.NoError(t, oneOfStringTestObjectASchema.ValidateCompatibility(validWithObjectC))
-	assert.Error(t, oneOfStringTestObjectASchema.ValidateCompatibility(invalidDiscriminatorType))
-	assert.Error(t, oneOfStringTestObjectASchema.ValidateCompatibility(invalidDiscriminator))
-	assert.NoError(t, oneOfStringTestObjectASchema.ValidateCompatibility(combinedMapAndSchema))
-	assert.Error(t, oneOfStringTestObjectASchema.ValidateCompatibility(combinedMapAndInvalidSchema))
+    combinedMapAndSchema := map[string]any{
+        "s": map[string]any{
+            "_type": "B",
+            // object B fields, but with a schema instead of a value
+            "message": schema.NewStringSchema(nil, nil, nil),
+        },
+    }
+    combinedMapAndInvalidSchema := map[string]any{
+        "s": map[string]any{
+            "_type": "B",
+            // object B fields, but with a schema instead of a value
+            "message": schema.NewIntSchema(nil, nil, nil),
+        },
+    }
+    assert.NoError(t, oneOfStringTestObjectASchema.ValidateCompatibility(validWithObjectB))
+    assert.NoError(t, oneOfStringTestObjectASchema.ValidateCompatibility(validWithObjectC))
+    assert.Error(t, oneOfStringTestObjectASchema.ValidateCompatibility(invalidDiscriminatorType))
+    assert.Error(t, oneOfStringTestObjectASchema.ValidateCompatibility(invalidDiscriminator))
+    assert.NoError(t, oneOfStringTestObjectASchema.ValidateCompatibility(combinedMapAndSchema))
+    assert.Error(t, oneOfStringTestObjectASchema.ValidateCompatibility(combinedMapAndInvalidSchema))
+}
+
+var oneOfNamePropertiesNoRefs = map[string]*schema.PropertySchema{
+    "name": schema.NewPropertySchema(
+        schema.NewOneOfStringSchema[any](
+            map[string]schema.Object{
+                "fullname": fullnameSchema,
+                "nickname": nicknameSchema,
+            },
+            "_type",
+        ),
+        nil,
+        true,
+        nil,
+        nil,
+        nil,
+        nil,
+        nil,
+    ),
+}
+
+var oneOfNameProperties = map[string]*schema.PropertySchema{
+    "name": schema.NewPropertySchema(
+        schema.NewOneOfStringSchema[any](
+            map[string]schema.Object{
+                "fullname": schema.NewRefSchema(
+                    "FullName",
+                    schema.NewDisplayValue(schema.PointerTo("FullName"), nil, nil)),
+                "nickname": schema.NewRefSchema(
+                    "Nickname",
+                    schema.NewDisplayValue(schema.PointerTo("Nickname"), nil, nil)),
+            },
+            "_type",
+        ),
+        nil,
+        true,
+        nil,
+        nil,
+        nil,
+        nil,
+        nil,
+    ),
+}
+
+var fullnameProperties = map[string]*schema.PropertySchema{
+    "first_name": schema.NewPropertySchema(
+        schema.NewStringSchema(nil, nil, nil),
+        schema.NewDisplayValue(schema.PointerTo("first_name"), nil, nil),
+        true,
+        nil,
+        nil,
+        nil,
+        nil,
+        nil,
+    ),
+    "last_name": schema.NewPropertySchema(
+        schema.NewStringSchema(nil, nil, nil),
+        schema.NewDisplayValue(schema.PointerTo("last_name"), nil, nil),
+        true,
+        nil,
+        nil,
+        nil,
+        nil,
+        nil,
+    ),
+    "middle": schema.NewPropertySchema(
+        schema.NewStringSchema(nil, nil, nil),
+        schema.NewDisplayValue(schema.PointerTo("middle"), nil, nil),
+        false,
+        nil,
+        nil,
+        nil,
+        nil,
+        nil,
+    ),
+}
+
+var nicknameProperties = map[string]*schema.PropertySchema{
+    "nick": schema.NewPropertySchema(
+        schema.NewStringSchema(nil, nil, nil),
+        schema.NewDisplayValue(schema.PointerTo("nick"), nil, nil),
+        true,
+        nil,
+        nil,
+        nil,
+        nil,
+        nil,
+    ),
+}
+
+var fullnameSchema = schema.NewObjectSchema(
+    "FullName",
+    fullnameProperties,
+)
+
+var nicknameSchema = schema.NewObjectSchema(
+    "Nickname",
+    nicknameProperties,
+)
+
+var oneOfNameRootScope = schema.NewScopeSchema(
+    schema.NewObjectSchema(
+        "RootObject",
+        oneOfNameProperties,
+    ),
+    fullnameSchema,
+    nicknameSchema,
+)
+
+var oneOfNameNoRefsRootScope = schema.NewScopeSchema(
+    schema.NewObjectSchema(
+        "RootObject",
+        oneOfNamePropertiesNoRefs,
+    ),
+    fullnameSchema,
+    nicknameSchema,
+)
+
+func TestOneOfString_Fullname(t *testing.T) {
+    var input_full any = map[string]any{
+        "name": map[string]any{
+            "_type":      "fullname",
+            "first_name": "Arca",
+            "last_name":  "Lot",
+        },
+    }
+    unserialized0, err := oneOfNameRootScope.Unserialize(input_full)
+    assert.NoError(t, err)
+    serialized, err := oneOfNameRootScope.Serialize(unserialized0)
+    assert.NoError(t, err)
+    unserialized, err := oneOfNameRootScope.Unserialize(input_full)
+    assert.NoError(t, err)
+    unserialized2, err := oneOfNameRootScope.Unserialize(serialized)
+    assert.Equals(t, unserialized2, unserialized)
+
 }
